cce77645381f71b1d39a445b54001b4b2a6e1dd3
RocketBoyGames <91237752+RocketBoyGames@users.noreply.github.com>
2022-02-09 20:54:02 +1100

    eula_fix2

--- code0.js
+++ code0.js
@@ -1,90 +1,133 @@
 gdjs.black_95screenCode = {};
 gdjs.black_95screenCode.GDEmilyObjects1= [];
 gdjs.black_95screenCode.GDEmilyObjects2= [];
+gdjs.black_95screenCode.GDEmilyObjects3= [];
 gdjs.black_95screenCode.GDPlayerObjects1= [];
 gdjs.black_95screenCode.GDPlayerObjects2= [];
+gdjs.black_95screenCode.GDPlayerObjects3= [];
 gdjs.black_95screenCode.GDbuttonsObjects1= [];
 gdjs.black_95screenCode.GDbuttonsObjects2= [];
+gdjs.black_95screenCode.GDbuttonsObjects3= [];
 gdjs.black_95screenCode.GDhungerThirstObjects1= [];
 gdjs.black_95screenCode.GDhungerThirstObjects2= [];
+gdjs.black_95screenCode.GDhungerThirstObjects3= [];
 gdjs.black_95screenCode.GDhotspots2Objects1= [];
 gdjs.black_95screenCode.GDhotspots2Objects2= [];
+gdjs.black_95screenCode.GDhotspots2Objects3= [];
 gdjs.black_95screenCode.GDhotspotsObjects1= [];
 gdjs.black_95screenCode.GDhotspotsObjects2= [];
+gdjs.black_95screenCode.GDhotspotsObjects3= [];
 gdjs.black_95screenCode.GDfollowPlayerObjects1= [];
 gdjs.black_95screenCode.GDfollowPlayerObjects2= [];
+gdjs.black_95screenCode.GDfollowPlayerObjects3= [];
 gdjs.black_95screenCode.GDcollisionsObjects1= [];
 gdjs.black_95screenCode.GDcollisionsObjects2= [];
+gdjs.black_95screenCode.GDcollisionsObjects3= [];
 gdjs.black_95screenCode.GDFGObjects1= [];
 gdjs.black_95screenCode.GDFGObjects2= [];
+gdjs.black_95screenCode.GDFGObjects3= [];
 gdjs.black_95screenCode.GDBGObjects1= [];
 gdjs.black_95screenCode.GDBGObjects2= [];
+gdjs.black_95screenCode.GDBGObjects3= [];
 gdjs.black_95screenCode.GDCursorObjects1= [];
 gdjs.black_95screenCode.GDCursorObjects2= [];
+gdjs.black_95screenCode.GDCursorObjects3= [];
 gdjs.black_95screenCode.GDIconbarHotspotObjects1= [];
 gdjs.black_95screenCode.GDIconbarHotspotObjects2= [];
+gdjs.black_95screenCode.GDIconbarHotspotObjects3= [];
 gdjs.black_95screenCode.GDempty_95slotObjects1= [];
 gdjs.black_95screenCode.GDempty_95slotObjects2= [];
+gdjs.black_95screenCode.GDempty_95slotObjects3= [];
 gdjs.black_95screenCode.GDinventory_95slotObjects1= [];
 gdjs.black_95screenCode.GDinventory_95slotObjects2= [];
+gdjs.black_95screenCode.GDinventory_95slotObjects3= [];
 gdjs.black_95screenCode.GDinventory_95maskObjects1= [];
 gdjs.black_95screenCode.GDinventory_95maskObjects2= [];
+gdjs.black_95screenCode.GDinventory_95maskObjects3= [];
 gdjs.black_95screenCode.GDinventory_95backgroundObjects1= [];
 gdjs.black_95screenCode.GDinventory_95backgroundObjects2= [];
+gdjs.black_95screenCode.GDinventory_95backgroundObjects3= [];
 gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects1= [];
 gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects2= [];
+gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects3= [];
 gdjs.black_95screenCode.GDhotspot_95nameObjects1= [];
 gdjs.black_95screenCode.GDhotspot_95nameObjects2= [];
+gdjs.black_95screenCode.GDhotspot_95nameObjects3= [];
 gdjs.black_95screenCode.GDnarrator_95text_95boxObjects1= [];
 gdjs.black_95screenCode.GDnarrator_95text_95boxObjects2= [];
+gdjs.black_95screenCode.GDnarrator_95text_95boxObjects3= [];
 gdjs.black_95screenCode.GDnarrator_95textObjects1= [];
 gdjs.black_95screenCode.GDnarrator_95textObjects2= [];
+gdjs.black_95screenCode.GDnarrator_95textObjects3= [];
 gdjs.black_95screenCode.GDtestTextObjects1= [];
 gdjs.black_95screenCode.GDtestTextObjects2= [];
+gdjs.black_95screenCode.GDtestTextObjects3= [];
 gdjs.black_95screenCode.GDtestText2Objects1= [];
 gdjs.black_95screenCode.GDtestText2Objects2= [];
+gdjs.black_95screenCode.GDtestText2Objects3= [];
 gdjs.black_95screenCode.GDregionsObjects1= [];
 gdjs.black_95screenCode.GDregionsObjects2= [];
+gdjs.black_95screenCode.GDregionsObjects3= [];
 gdjs.black_95screenCode.GDplayer_95dialogueObjects1= [];
 gdjs.black_95screenCode.GDplayer_95dialogueObjects2= [];
+gdjs.black_95screenCode.GDplayer_95dialogueObjects3= [];
 gdjs.black_95screenCode.GDexternal_95popupsObjects1= [];
 gdjs.black_95screenCode.GDexternal_95popupsObjects2= [];
+gdjs.black_95screenCode.GDexternal_95popupsObjects3= [];
 gdjs.black_95screenCode.GDmission5Objects1= [];
 gdjs.black_95screenCode.GDmission5Objects2= [];
+gdjs.black_95screenCode.GDmission5Objects3= [];
 gdjs.black_95screenCode.GDmission4Objects1= [];
 gdjs.black_95screenCode.GDmission4Objects2= [];
+gdjs.black_95screenCode.GDmission4Objects3= [];
 gdjs.black_95screenCode.GDmission3Objects1= [];
 gdjs.black_95screenCode.GDmission3Objects2= [];
+gdjs.black_95screenCode.GDmission3Objects3= [];
 gdjs.black_95screenCode.GDmission2Objects1= [];
 gdjs.black_95screenCode.GDmission2Objects2= [];
+gdjs.black_95screenCode.GDmission2Objects3= [];
 gdjs.black_95screenCode.GDmission1Objects1= [];
 gdjs.black_95screenCode.GDmission1Objects2= [];
+gdjs.black_95screenCode.GDmission1Objects3= [];
 gdjs.black_95screenCode.GDsave_95noticeObjects1= [];
 gdjs.black_95screenCode.GDsave_95noticeObjects2= [];
+gdjs.black_95screenCode.GDsave_95noticeObjects3= [];
 gdjs.black_95screenCode.GDOptionsObjects1= [];
 gdjs.black_95screenCode.GDOptionsObjects2= [];
+gdjs.black_95screenCode.GDOptionsObjects3= [];
 gdjs.black_95screenCode.GDsliderMusicObjects1= [];
 gdjs.black_95screenCode.GDsliderMusicObjects2= [];
+gdjs.black_95screenCode.GDsliderMusicObjects3= [];
 gdjs.black_95screenCode.GDsliderGlobalObjects1= [];
 gdjs.black_95screenCode.GDsliderGlobalObjects2= [];
+gdjs.black_95screenCode.GDsliderGlobalObjects3= [];
 gdjs.black_95screenCode.GDcheckboxFullscreenObjects1= [];
 gdjs.black_95screenCode.GDcheckboxFullscreenObjects2= [];
+gdjs.black_95screenCode.GDcheckboxFullscreenObjects3= [];
 gdjs.black_95screenCode.GDcheckboxGameTextObjects1= [];
 gdjs.black_95screenCode.GDcheckboxGameTextObjects2= [];
+gdjs.black_95screenCode.GDcheckboxGameTextObjects3= [];
 gdjs.black_95screenCode.GDcheckboxFullscreennObjects1= [];
 gdjs.black_95screenCode.GDcheckboxFullscreennObjects2= [];
+gdjs.black_95screenCode.GDcheckboxFullscreennObjects3= [];
 gdjs.black_95screenCode.GDscoreObjects1= [];
 gdjs.black_95screenCode.GDscoreObjects2= [];
+gdjs.black_95screenCode.GDscoreObjects3= [];
 gdjs.black_95screenCode.GDcheckboxGameTexttObjects1= [];
 gdjs.black_95screenCode.GDcheckboxGameTexttObjects2= [];
+gdjs.black_95screenCode.GDcheckboxGameTexttObjects3= [];
 gdjs.black_95screenCode.GDcreditsObjects1= [];
 gdjs.black_95screenCode.GDcreditsObjects2= [];
+gdjs.black_95screenCode.GDcreditsObjects3= [];
 gdjs.black_95screenCode.GDdialogue_95hudObjects1= [];
 gdjs.black_95screenCode.GDdialogue_95hudObjects2= [];
+gdjs.black_95screenCode.GDdialogue_95hudObjects3= [];
 gdjs.black_95screenCode.GDdialogue_95optionssObjects1= [];
 gdjs.black_95screenCode.GDdialogue_95optionssObjects2= [];
+gdjs.black_95screenCode.GDdialogue_95optionssObjects3= [];
 gdjs.black_95screenCode.GDline_95list1Objects1= [];
 gdjs.black_95screenCode.GDline_95list1Objects2= [];
+gdjs.black_95screenCode.GDline_95list1Objects3= [];
 
 gdjs.black_95screenCode.conditionTrue_0 = {val:false};
 gdjs.black_95screenCode.condition0IsTrue_0 = {val:false};
@@ -97,27 +140,6 @@ gdjs.black_95screenCode.condition2IsTrue_1 = {val:false};
 
 
 gdjs.black_95screenCode.eventsList0 = function(runtimeScene) {
-
-{
-
-
-gdjs.black_95screenCode.condition0IsTrue_0.val = false;
-gdjs.black_95screenCode.condition1IsTrue_0.val = false;
-{
-gdjs.black_95screenCode.condition0IsTrue_0.val = gdjs.evtTools.storage.elementExistsInJSONFile("saved_games", "settings");
-}if ( gdjs.black_95screenCode.condition0IsTrue_0.val ) {
-{
-{gdjs.black_95screenCode.conditionTrue_1 = gdjs.black_95screenCode.condition1IsTrue_0;
-gdjs.black_95screenCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(11871380);
-}
-}}
-if (gdjs.black_95screenCode.condition1IsTrue_0.val) {
-{gdjs.evtTools.storage.readStringFromJSONFile("saved_games", "settings", runtimeScene, runtimeScene.getVariables().get("_tempSetings"));
-}{gdjs.evtTools.network.jsonToVariableStructure(gdjs.evtTools.variable.getVariableString(runtimeScene.getVariables().get("_tempSetings")), runtimeScene.getGame().getVariables().getFromIndex(27));
-}}
-
-}
-
 
 {
 
@@ -164,7 +186,52 @@ if (gdjs.black_95screenCode.condition1IsTrue_0.val) {
 {
 
 
+gdjs.black_95screenCode.condition0IsTrue_0.val = false;
 {
+{gdjs.black_95screenCode.conditionTrue_1 = gdjs.black_95screenCode.condition0IsTrue_0;
+gdjs.black_95screenCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(42215036);
+}
+}if (gdjs.black_95screenCode.condition0IsTrue_0.val) {
+{gdjs.evtTools.runtimeScene.pauseTimer(runtimeScene, "next_room");
+}}
+
+}
+
+
+};gdjs.black_95screenCode.eventsList1 = function(runtimeScene) {
+
+{
+
+
+gdjs.black_95screenCode.condition0IsTrue_0.val = false;
+gdjs.black_95screenCode.condition1IsTrue_0.val = false;
+{
+gdjs.black_95screenCode.condition0IsTrue_0.val = gdjs.evtTools.storage.elementExistsInJSONFile("saved_games", "settings");
+}if ( gdjs.black_95screenCode.condition0IsTrue_0.val ) {
+{
+{gdjs.black_95screenCode.conditionTrue_1 = gdjs.black_95screenCode.condition1IsTrue_0;
+gdjs.black_95screenCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(11871380);
+}
+}}
+if (gdjs.black_95screenCode.condition1IsTrue_0.val) {
+{gdjs.evtTools.storage.readStringFromJSONFile("saved_games", "settings", runtimeScene, runtimeScene.getVariables().get("_tempSetings"));
+}{gdjs.evtTools.network.jsonToVariableStructure(gdjs.evtTools.variable.getVariableString(runtimeScene.getVariables().get("_tempSetings")), runtimeScene.getGame().getVariables().getFromIndex(27));
+}{gdjs.evtTools.runtimeScene.resetTimer(runtimeScene, "next_room");
+}}
+
+}
+
+
+{
+
+
+gdjs.black_95screenCode.condition0IsTrue_0.val = false;
+{
+gdjs.black_95screenCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.timerElapsedTime(runtimeScene, 1, "next_room");
+}if (gdjs.black_95screenCode.condition0IsTrue_0.val) {
+
+{ //Subevents
+gdjs.black_95screenCode.eventsList0(runtimeScene);} //End of subevents
 }
 
 }
@@ -177,92 +244,135 @@ runtimeScene.getOnceTriggers().startNewFrame();
 
 gdjs.black_95screenCode.GDEmilyObjects1.length = 0;
 gdjs.black_95screenCode.GDEmilyObjects2.length = 0;
+gdjs.black_95screenCode.GDEmilyObjects3.length = 0;
 gdjs.black_95screenCode.GDPlayerObjects1.length = 0;
 gdjs.black_95screenCode.GDPlayerObjects2.length = 0;
+gdjs.black_95screenCode.GDPlayerObjects3.length = 0;
 gdjs.black_95screenCode.GDbuttonsObjects1.length = 0;
 gdjs.black_95screenCode.GDbuttonsObjects2.length = 0;
+gdjs.black_95screenCode.GDbuttonsObjects3.length = 0;
 gdjs.black_95screenCode.GDhungerThirstObjects1.length = 0;
 gdjs.black_95screenCode.GDhungerThirstObjects2.length = 0;
+gdjs.black_95screenCode.GDhungerThirstObjects3.length = 0;
 gdjs.black_95screenCode.GDhotspots2Objects1.length = 0;
 gdjs.black_95screenCode.GDhotspots2Objects2.length = 0;
+gdjs.black_95screenCode.GDhotspots2Objects3.length = 0;
 gdjs.black_95screenCode.GDhotspotsObjects1.length = 0;
 gdjs.black_95screenCode.GDhotspotsObjects2.length = 0;
+gdjs.black_95screenCode.GDhotspotsObjects3.length = 0;
 gdjs.black_95screenCode.GDfollowPlayerObjects1.length = 0;
 gdjs.black_95screenCode.GDfollowPlayerObjects2.length = 0;
+gdjs.black_95screenCode.GDfollowPlayerObjects3.length = 0;
 gdjs.black_95screenCode.GDcollisionsObjects1.length = 0;
 gdjs.black_95screenCode.GDcollisionsObjects2.length = 0;
+gdjs.black_95screenCode.GDcollisionsObjects3.length = 0;
 gdjs.black_95screenCode.GDFGObjects1.length = 0;
 gdjs.black_95screenCode.GDFGObjects2.length = 0;
+gdjs.black_95screenCode.GDFGObjects3.length = 0;
 gdjs.black_95screenCode.GDBGObjects1.length = 0;
 gdjs.black_95screenCode.GDBGObjects2.length = 0;
+gdjs.black_95screenCode.GDBGObjects3.length = 0;
 gdjs.black_95screenCode.GDCursorObjects1.length = 0;
 gdjs.black_95screenCode.GDCursorObjects2.length = 0;
+gdjs.black_95screenCode.GDCursorObjects3.length = 0;
 gdjs.black_95screenCode.GDIconbarHotspotObjects1.length = 0;
 gdjs.black_95screenCode.GDIconbarHotspotObjects2.length = 0;
+gdjs.black_95screenCode.GDIconbarHotspotObjects3.length = 0;
 gdjs.black_95screenCode.GDempty_95slotObjects1.length = 0;
 gdjs.black_95screenCode.GDempty_95slotObjects2.length = 0;
+gdjs.black_95screenCode.GDempty_95slotObjects3.length = 0;
 gdjs.black_95screenCode.GDinventory_95slotObjects1.length = 0;
 gdjs.black_95screenCode.GDinventory_95slotObjects2.length = 0;
+gdjs.black_95screenCode.GDinventory_95slotObjects3.length = 0;
 gdjs.black_95screenCode.GDinventory_95maskObjects1.length = 0;
 gdjs.black_95screenCode.GDinventory_95maskObjects2.length = 0;
+gdjs.black_95screenCode.GDinventory_95maskObjects3.length = 0;
 gdjs.black_95screenCode.GDinventory_95backgroundObjects1.length = 0;
 gdjs.black_95screenCode.GDinventory_95backgroundObjects2.length = 0;
+gdjs.black_95screenCode.GDinventory_95backgroundObjects3.length = 0;
 gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects1.length = 0;
 gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects2.length = 0;
+gdjs.black_95screenCode.GDhotspot_95name_95cursorObjects3.length = 0;
 gdjs.black_95screenCode.GDhotspot_95nameObjects1.length = 0;
 gdjs.black_95screenCode.GDhotspot_95nameObjects2.length = 0;
+gdjs.black_95screenCode.GDhotspot_95nameObjects3.length = 0;
 gdjs.black_95screenCode.GDnarrator_95text_95boxObjects1.length = 0;
 gdjs.black_95screenCode.GDnarrator_95text_95boxObjects2.length = 0;
+gdjs.black_95screenCode.GDnarrator_95text_95boxObjects3.length = 0;
 gdjs.black_95screenCode.GDnarrator_95textObjects1.length = 0;
 gdjs.black_95screenCode.GDnarrator_95textObjects2.length = 0;
+gdjs.black_95screenCode.GDnarrator_95textObjects3.length = 0;
 gdjs.black_95screenCode.GDtestTextObjects1.length = 0;
 gdjs.black_95screenCode.GDtestTextObjects2.length = 0;
+gdjs.black_95screenCode.GDtestTextObjects3.length = 0;
 gdjs.black_95screenCode.GDtestText2Objects1.length = 0;
 gdjs.black_95screenCode.GDtestText2Objects2.length = 0;
+gdjs.black_95screenCode.GDtestText2Objects3.length = 0;
 gdjs.black_95screenCode.GDregionsObjects1.length = 0;
 gdjs.black_95screenCode.GDregionsObjects2.length = 0;
+gdjs.black_95screenCode.GDregionsObjects3.length = 0;
 gdjs.black_95screenCode.GDplayer_95dialogueObjects1.length = 0;
 gdjs.black_95screenCode.GDplayer_95dialogueObjects2.length = 0;
+gdjs.black_95screenCode.GDplayer_95dialogueObjects3.length = 0;
 gdjs.black_95screenCode.GDexternal_95popupsObjects1.length = 0;
 gdjs.black_95screenCode.GDexternal_95popupsObjects2.length = 0;
+gdjs.black_95screenCode.GDexternal_95popupsObjects3.length = 0;
 gdjs.black_95screenCode.GDmission5Objects1.length = 0;
 gdjs.black_95screenCode.GDmission5Objects2.length = 0;
+gdjs.black_95screenCode.GDmission5Objects3.length = 0;
 gdjs.black_95screenCode.GDmission4Objects1.length = 0;
 gdjs.black_95screenCode.GDmission4Objects2.length = 0;
+gdjs.black_95screenCode.GDmission4Objects3.length = 0;
 gdjs.black_95screenCode.GDmission3Objects1.length = 0;
 gdjs.black_95screenCode.GDmission3Objects2.length = 0;
+gdjs.black_95screenCode.GDmission3Objects3.length = 0;
 gdjs.black_95screenCode.GDmission2Objects1.length = 0;
 gdjs.black_95screenCode.GDmission2Objects2.length = 0;
+gdjs.black_95screenCode.GDmission2Objects3.length = 0;
 gdjs.black_95screenCode.GDmission1Objects1.length = 0;
 gdjs.black_95screenCode.GDmission1Objects2.length = 0;
+gdjs.black_95screenCode.GDmission1Objects3.length = 0;
 gdjs.black_95screenCode.GDsave_95noticeObjects1.length = 0;
 gdjs.black_95screenCode.GDsave_95noticeObjects2.length = 0;
+gdjs.black_95screenCode.GDsave_95noticeObjects3.length = 0;
 gdjs.black_95screenCode.GDOptionsObjects1.length = 0;
 gdjs.black_95screenCode.GDOptionsObjects2.length = 0;
+gdjs.black_95screenCode.GDOptionsObjects3.length = 0;
 gdjs.black_95screenCode.GDsliderMusicObjects1.length = 0;
 gdjs.black_95screenCode.GDsliderMusicObjects2.length = 0;
+gdjs.black_95screenCode.GDsliderMusicObjects3.length = 0;
 gdjs.black_95screenCode.GDsliderGlobalObjects1.length = 0;
 gdjs.black_95screenCode.GDsliderGlobalObjects2.length = 0;
+gdjs.black_95screenCode.GDsliderGlobalObjects3.length = 0;
 gdjs.black_95screenCode.GDcheckboxFullscreenObjects1.length = 0;
 gdjs.black_95screenCode.GDcheckboxFullscreenObjects2.length = 0;
+gdjs.black_95screenCode.GDcheckboxFullscreenObjects3.length = 0;
 gdjs.black_95screenCode.GDcheckboxGameTextObjects1.length = 0;
 gdjs.black_95screenCode.GDcheckboxGameTextObjects2.length = 0;
+gdjs.black_95screenCode.GDcheckboxGameTextObjects3.length = 0;
 gdjs.black_95screenCode.GDcheckboxFullscreennObjects1.length = 0;
 gdjs.black_95screenCode.GDcheckboxFullscreennObjects2.length = 0;
+gdjs.black_95screenCode.GDcheckboxFullscreennObjects3.length = 0;
 gdjs.black_95screenCode.GDscoreObjects1.length = 0;
 gdjs.black_95screenCode.GDscoreObjects2.length = 0;
+gdjs.black_95screenCode.GDscoreObjects3.length = 0;
 gdjs.black_95screenCode.GDcheckboxGameTexttObjects1.length = 0;
 gdjs.black_95screenCode.GDcheckboxGameTexttObjects2.length = 0;
+gdjs.black_95screenCode.GDcheckboxGameTexttObjects3.length = 0;
 gdjs.black_95screenCode.GDcreditsObjects1.length = 0;
 gdjs.black_95screenCode.GDcreditsObjects2.length = 0;
+gdjs.black_95screenCode.GDcreditsObjects3.length = 0;
 gdjs.black_95screenCode.GDdialogue_95hudObjects1.length = 0;
 gdjs.black_95screenCode.GDdialogue_95hudObjects2.length = 0;
+gdjs.black_95screenCode.GDdialogue_95hudObjects3.length = 0;
 gdjs.black_95screenCode.GDdialogue_95optionssObjects1.length = 0;
 gdjs.black_95screenCode.GDdialogue_95optionssObjects2.length = 0;
+gdjs.black_95screenCode.GDdialogue_95optionssObjects3.length = 0;
 gdjs.black_95screenCode.GDline_95list1Objects1.length = 0;
 gdjs.black_95screenCode.GDline_95list1Objects2.length = 0;
+gdjs.black_95screenCode.GDline_95list1Objects3.length = 0;
 
-gdjs.black_95screenCode.eventsList0(runtimeScene);
+gdjs.black_95screenCode.eventsList1(runtimeScene);
 return;
 
 }
